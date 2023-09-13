868eb00362a68b3c250baad45a5ad71a763014cf
percivalho <35653644+percivalho@users.noreply.github.com>
2023-09-13 20:06:16 +0100

    more $ sign to £ sign

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -35,7 +35,7 @@ function OrderHistory() {
                         <p>{name}</p>
                       </Link>
                       <div>
-                        <span>${price}</span>
+                        <span>£{price}</span>
                       </div>
                     </div>
                   ))}
